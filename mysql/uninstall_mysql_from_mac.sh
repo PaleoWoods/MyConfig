a6d9410a3d55baa98d256162b84dddc7b1cdfa87
zhouyong <zhouyong@huoqiu.cn>
2016-05-23 18:55:20 +0800

    add uninstall mysql from mac

---
+++ mysql/uninstall_mysql_from_mac.sh
@@ -0,0 +1,11 @@
+#!/usr/bash
+sudo rm /usr/local/mysql
+sudo rm -rf /usr/local/mysql*
+sudo rm -rf /Library/StartupItems/MySQLCOM
+sudo rm -rf /Library/PreferencePanes/My*
+# vim /etc/hostconfig  (and removed the line MYSQLCOM=-YES-)
+sudo sed -i -e '/MYSQLCOM=-YES-/d' etc/hostconfig  
+rm -rf ~/Library/PreferencePanes/My*
+sudo rm -rf /Library/Receipts/mysql*
+sudo rm -rf /Library/Receipts/MySQL*
+sudo rm -rf /var/db/receipts/com.mysql.*
